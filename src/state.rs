47b199a9886f225618a4005b9d7dbed26ac96ee7
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2025-11-29 22:59:34 +0100

    Added Time Buffer and Compute
    Introduces Uniform time buffer, start instant and last time needed to give dt to compute shader. Later this will be needed to calculate the integral for Reaction-Diffusion system. Moreover compute shader, compute bind group layout, compute bind group, compute pipeline layout and compute pipeline are all added to the State. Function render is renamed now to compute_and_render since it also hosts compute pass as well as rendering. The rendering here will be replaced at some point with vertex and framgment shaders

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,22 @@
-use wgpu::{wgt::CommandEncoderDescriptor, *};
+use std::{num::NonZeroU64, time::Instant};
+
+use bytemuck::{Pod, Zeroable};
+use wgpu::{
+    util::{BufferInitDescriptor, DeviceExt},
+    wgt::CommandEncoderDescriptor,
+    *,
+};
 use winit::{dpi::PhysicalSize, window::Window};
 
+// time
+#[repr(C)] // format expected by the gpu
+#[derive(Clone, Copy, Pod, Zeroable)]
+struct TimeUniform {
+    // 16 byte alignment needed
+    dt: f32,        // 4 byte
+    _pad: [f32; 3], // 12 byte
+}
+// connection to gpu
 pub struct State {
     // includes communication steps with GPU
     // making the window, openning connection to GPU
@@ -9,6 +25,16 @@ pub struct State {
     pub queue: Queue,
     pub config: SurfaceConfiguration,
     pub size: PhysicalSize<u32>,
+
+    // uniform
+    pub time_buffer: Buffer,
+    pub start_instant: Instant,
+    pub last_time: f32,
+
+    // compute
+    pub compute_bgl: BindGroupLayout,
+    pub compute_bg: BindGroup,
+    pub compute_pipeline: ComputePipeline,
 }
 
 impl State {
@@ -69,12 +95,81 @@ impl State {
 
         surface_m.configure(&device_m, &config_m);
 
+        // time uniform buffer
+        let time_uniform = TimeUniform {
+            dt: 0.0,
+            _pad: [0.0; 3],
+        };
+
+        let time_buffer = device_m.create_buffer_init(&BufferInitDescriptor {
+            label: Some("Time Uniform Buffer"),
+            contents: bytemuck::bytes_of(&time_uniform),
+            usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
+        });
+
+        let start_instant = Instant::now();
+        let last_time: f32 = 0.0;
+
+        // shader modules
+        let compute_shader = device_m.create_shader_module(ShaderModuleDescriptor {
+            label: Some("Compute Shder Module"),
+            source: ShaderSource::Wgsl(include_str!("../shaders/rd_compute.wgsl").into()),
+        });
+
+        // compute
+        let compute_bgl = device_m.create_bind_group_layout(&BindGroupLayoutDescriptor {
+            label: Some("Compute Bing Group Layout"),
+            entries: &[BindGroupLayoutEntry {
+                // time uniform buffer binding 0
+                binding: 0,
+                visibility: ShaderStages::COMPUTE,
+                ty: BindingType::Buffer {
+                    ty: BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: NonZeroU64::new(std::mem::size_of::<TimeUniform>() as u64),
+                },
+                count: None,
+            }],
+        });
+
+        let compute_bg = device_m.create_bind_group(&BindGroupDescriptor {
+            label: Some("Compute Bind Group"),
+            layout: &compute_bgl,
+            entries: &[BindGroupEntry {
+                binding: 0,
+                resource: time_buffer.as_entire_binding(),
+            }],
+        });
+
+        let compute_pipeline_layout = device_m.create_pipeline_layout(&PipelineLayoutDescriptor {
+            label: Some("Compute Pipeline Layout"),
+            bind_group_layouts: &[&compute_bgl],
+            push_constant_ranges: &[],
+        });
+
+        let compute_pipeline = device_m.create_compute_pipeline(&ComputePipelineDescriptor {
+            label: Some("Compute Pipeline"),
+            layout: Some(&compute_pipeline_layout),
+            module: &compute_shader,
+            entry_point: Some("main"),
+            compilation_options: PipelineCompilationOptions::default(),
+            cache: None,
+        });
+
         Ok(Self {
             surface: surface_m,
             device: device_m,
             queue: queue_m,
             config: config_m,
             size,
+
+            time_buffer,
+            start_instant,
+            last_time,
+
+            compute_bgl,
+            compute_bg,
+            compute_pipeline,
         })
     }
 
@@ -88,7 +183,7 @@ impl State {
         }
     }
 
-    pub fn render(&mut self) -> Result<(), SurfaceError> {
+    pub fn compute_and_render(&mut self) -> Result<(), SurfaceError> {
         let frame = self.surface.get_current_texture()?;
         let view = frame.texture.create_view(&TextureViewDescriptor::default());
 
@@ -98,7 +193,31 @@ impl State {
                 label: Some("Encoder"),
             });
 
-        // opening a new scope because we cannot submit to the queue in the same scope
+        // update dt
+        let now = self.start_instant.elapsed().as_secs_f32();
+        let dt = (now - self.last_time).max(0.0);
+        self.last_time = now;
+
+        let time_uniform = TimeUniform { dt, _pad: [0.0; 3] };
+
+        self.queue
+            .write_buffer(&self.time_buffer, 0, bytemuck::bytes_of(&time_uniform));
+
+        // compute pass scope
+        {
+            let mut cpass = encoder.begin_compute_pass(&ComputePassDescriptor {
+                label: Some("Compute Pass"),
+                timestamp_writes: None,
+            });
+
+            cpass.set_pipeline(&self.compute_pipeline);
+            cpass.set_bind_group(0, &self.compute_bg, &[]);
+
+            let workgroup = (256 + 255) / 256; // TODO (WG + (N - 1) / N) Need to be changed later accordigly
+            cpass.dispatch_workgroups(workgroup, 1, 1);
+        }
+
+        // render pass scope
         {
             let _rpass = encoder.begin_render_pass(&RenderPassDescriptor {
                 label: Some("Render Pass"),
