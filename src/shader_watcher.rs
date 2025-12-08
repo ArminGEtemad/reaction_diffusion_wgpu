d3751207082a3cba8bfa4e89f38cedd412fac872
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2025-12-08 22:07:17 +0100

    Made into convetional Rust Notation
    There is no change in the code other than make the code more readable using rust-analyzer.

--- src/shader_watcher.rs
+++ src/shader_watcher.rs
@@ -1,6 +1,11 @@
-use std::{path::PathBuf, sync::mpsc::{Receiver, channel}};
+use std::{
+    path::PathBuf,
+    sync::mpsc::{Receiver, channel},
+};
 
-use notify::{Event, EventKind, RecommendedWatcher, Watcher, event::ModifyKind, recommended_watcher};
+use notify::{
+    Event, EventKind, RecommendedWatcher, Watcher, event::ModifyKind, recommended_watcher,
+};
 
 pub struct ShaderWatcher {
     _watcher: RecommendedWatcher,
@@ -12,25 +17,27 @@ impl ShaderWatcher {
         let path = path.into();
         let (sender_x, reciever_x) = channel();
 
-        let mut watcher = recommended_watcher(
-            move | res: Result<Event, notify::Error>| match res {
-                Ok(event) => {
-                    if let EventKind::Modify(ModifyKind::Data(_)) = event.kind {
-                        for p in event.paths {
-                            let _ = sender_x.send(p);
-                        }
+        let mut watcher = recommended_watcher(move |res: Result<Event, notify::Error>| match res {
+            Ok(event) => {
+                if let EventKind::Modify(ModifyKind::Data(_)) = event.kind {
+                    for p in event.paths {
+                        let _ = sender_x.send(p);
                     }
                 }
-                Err(e) => {
-                    eprintln!("Watcher has the Error: {:?}", e);
-                }
             }
-        ).expect("Failed to create the watcher!");
+            Err(e) => {
+                eprintln!("Watcher has the Error: {:?}", e);
+            }
+        })
+        .expect("Failed to create the watcher!");
 
         watcher
             .watch(&path, notify::RecursiveMode::NonRecursive)
             .expect("Failed to watch the shader path!");
 
-        Self { _watcher: watcher, reciever_x }
+        Self {
+            _watcher: watcher,
+            reciever_x,
+        }
     }
 }
