77611b6b4b9f109270f2d2df61a752d0c02dd7ed
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2025-12-17 00:47:33 +0100

    Introducing RenderNode
    Here PerFrameParameters live as well as the trait RenderNode which defines the contract for every node. For now every node needs to have a name for debugging, they should be able to be executed, the should allow hot reload (later I want to add post processing and some filters that must also allow for hot reload just like the ReactionDiffusion itself). Also they must allow for downcasting so that we can access some of the funcions.

---
+++ src/render_graph/node.rs
@@ -0,0 +1,34 @@
+use std::any::Any;
+
+use crate::{
+    gpu_resources::{FrameContext, GpuResource},
+    render_graph::resource_registry::ResourceRegistry,
+};
+
+pub struct PerFrameParameters {
+    pub mouse_pos: Option<(f32, f32)>,
+    pub mouse_down: bool,
+    pub brush_radius: f32,
+    pub mode: u32,
+    pub paused: bool,
+    pub debug_mode: bool,
+}
+
+pub trait RenderNode {
+    fn name(&self) -> &str;
+
+    // execure every frame
+    fn execute(
+        &mut self,
+        registry: &ResourceRegistry,
+        gpu_res: &GpuResource,
+        frame: &mut FrameContext,
+        per_frame_parames: &PerFrameParameters,
+    );
+
+    // called when shaders are reloded
+    fn called_on_hotreload(&mut self, _gpu_res: &GpuResource) {}
+
+    // need it for the graph to be able to get access to any node I want in other scripts
+    fn as_any(&mut self) -> &mut dyn Any;
+}
