5be87782d9cbc6f1776439dc9f213d55f13b9e08
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2025-12-17 22:57:59 +0100

    Separating SimNode from DisplayNode
    ReactionDiffusionNode is now separated into ReactionDiffusionSimulationNode and ReactionDiffusionDisplayNode. Since they share same data I made a type called ReactionDiffusionShared so Both of the new nodes can be owners of the shared data. Brush Still Doesn't have its own node though and shares the node with Simulation. Hot reload function right now rebuilds the whole pipeline and doesn't separates the render and compute pipeline from each other right now. 'I have to test for the bugs there actually'

--- src/nodes/rd_node.rs
+++ src/nodes/rd_node.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, rc::Rc};
+
 use crate::{
     gpu_resources::{FrameContext, GpuResource},
     rd_system::{BrushUniform, ReactionDiffusionSystem, StartingPattern},
@@ -7,24 +9,45 @@ use crate::{
     },
 };
 
-pub struct ReactionDiffusionNode {
-    rd_system: ReactionDiffusionSystem,
+// RC allows for different owners of the same data
+// RefCell allows to mutate data even if they are immutable
+type ReactionDiffusionShared = Rc<RefCell<ReactionDiffusionSystem>>;
+
+pub struct ReactionDiffusionSimulationNode {
+    rd_shared: ReactionDiffusionShared,
 }
 
-impl ReactionDiffusionNode {
-    pub fn new(gpu_res: &GpuResource) -> Self {
-        let rd_system = ReactionDiffusionSystem::new(gpu_res);
-        Self { rd_system }
-    }
+pub struct ReactionDiffusionDisplayNode {
+    rd_shared: ReactionDiffusionShared,
+}
 
+pub fn create_rd_shared_nodes(
+    gpu_res: &GpuResource,
+) -> (
+    ReactionDiffusionSimulationNode,
+    ReactionDiffusionDisplayNode,
+) {
+    let shared = Rc::new(RefCell::new(ReactionDiffusionSystem::new(gpu_res)));
+    let sim = ReactionDiffusionSimulationNode {
+        rd_shared: Rc::clone(&shared),
+    };
+    let display = ReactionDiffusionDisplayNode { rd_shared: shared };
+
+    (sim, display)
+}
+
+impl ReactionDiffusionSimulationNode {
     pub fn reset(&mut self, gpu_res: &GpuResource, pattern: StartingPattern) {
-        self.rd_system.reset_and_rerun(gpu_res, pattern);
+        self.rd_shared
+            .borrow_mut()
+            .reset_and_rerun(gpu_res, pattern);
     }
 }
 
-impl RenderNode for ReactionDiffusionNode {
+// Compute Only (+ mouse injection)
+impl RenderNode for ReactionDiffusionSimulationNode {
     fn name(&self) -> &str {
-        "Reaction Diffusion Node"
+        "Reaction Diffusion Node (Simulation)"
     }
 
     fn execute(
@@ -34,7 +57,11 @@ impl RenderNode for ReactionDiffusionNode {
         frame: &mut FrameContext,
         per_frame_parames: &PerFrameParameters,
     ) {
-        let (w_rd, h_rd) = self.rd_system.rd_size();
+        let (w_rd, h_rd) = {
+            // new scope
+            let rd_shared = self.rd_shared.borrow();
+            rd_shared.rd_size()
+        };
 
         // brush input
         // Brush doesn't live in state anymore but in execute
@@ -68,14 +95,51 @@ impl RenderNode for ReactionDiffusionNode {
         }
 
         // upload the brush uniform
-        self.rd_system.set_brush_parameters(gpu_res, &brush_uniform);
+        {
+            let rd_shared = self.rd_shared.borrow();
+            rd_shared.set_brush_parameters(gpu_res, &brush_uniform);
+        }
 
-        self.rd_system
-            .compute_and_render_pass(gpu_res, frame, per_frame_parames.paused);
+        // do the compute
+        {
+            let mut rd_shared = self.rd_shared.borrow_mut();
+            rd_shared.step_simulation(gpu_res, frame, per_frame_parames.paused);
+        }
     }
 
     fn called_on_hotreload(&mut self, gpu_res: &GpuResource) {
-        self.rd_system.rebuild_pipeline(gpu_res);
+        // TODO the pipeline rebuild does the whole pipeline rebuild compute and render
+        // would that be a problem if I don't separate them?
+        self.rd_shared.borrow_mut().rebuild_pipeline(gpu_res);
+    }
+
+    fn as_any(&mut self) -> &mut dyn std::any::Any {
+        self
+    }
+}
+
+// Render
+impl RenderNode for ReactionDiffusionDisplayNode {
+    fn name(&self) -> &str {
+        "Reaction Diffusion Node (Display)"
+    }
+
+    fn execute(
+        &mut self,
+        _registry: &ResourceRegistry,
+        _gpu_res: &GpuResource,
+        frame: &mut FrameContext,
+        _per_frame_parames: &PerFrameParameters,
+    ) {
+        // it just renders actually
+        let rd_shared = self.rd_shared.borrow();
+
+        let view = frame.view.clone();
+        rd_shared.step_render(frame, &view);
+    }
+
+    fn called_on_hotreload(&mut self, _gpu_res: &GpuResource) {
+        // already done in compute one
     }
 
     fn as_any(&mut self) -> &mut dyn std::any::Any {
