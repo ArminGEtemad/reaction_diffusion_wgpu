aab5174a51d0c64ccaea03a76afe4afc68b8f458
Armin Ghorbanietemad <a.ghetemad@gmail.com>
2026-01-02 00:01:01 +0100

    Added consts for the second rd simulation

--- src/nodes/consts.rs
+++ src/nodes/consts.rs
@@ -1,8 +1,13 @@
 // texture names for referencing
-pub const TEX_RD_PING: &str = "rd ping";
-pub const TEX_RD_PONG: &str = "rd pong";
-pub const TEX_RD_TEMP: &str = "rd temp";
-pub const TEX_RD_OUTPUT: &str = "rd output";
+pub const TEX_RD1_PING: &str = "rd1 ping";
+pub const TEX_RD1_PONG: &str = "rd1 pong";
+pub const TEX_RD1_TEMP: &str = "rd1 temp";
+pub const TEX_RD1_OUTPUT: &str = "rd1 output";
+
+pub const TEX_RD2_PING: &str = "rd2 ping";
+pub const TEX_RD2_PONG: &str = "rd2 pong";
+pub const TEX_RD2_TEMP: &str = "rd2 temp";
+pub const TEX_RD2_OUTPUT: &str = "rd2 output";
 
 // workgroup size
 pub const WG_X: u32 = 16;
